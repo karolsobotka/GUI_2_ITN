c2f291ef9b37bb2bd5f5cbccafe318bb27b76936
karol <karolsobotka@protonmail.com>
2022-06-06 22:01:12 +0200

    further changes in management screen.

--- src/GUI/ManagementScreen.java
+++ src/GUI/ManagementScreen.java
@@ -1,6 +1,7 @@
 package GUI;
 
 import Project.Project;
+import Project.ProjectState;
 
 import javax.swing.*;
 import java.awt.*;
@@ -22,28 +23,38 @@ public class ManagementScreen extends JFrame {
 
     JPanel panelListyProjektow = new JPanel();
     JScrollPane scrollPaneProjektow = new JScrollPane();
+    JPanel decisionPanel = new JPanel();
     DefaultListModel<Project> model = new DefaultListModel<>();
     JList listaProjektow = new JList(model);
-    String [] projectStates = {"Active", "Planned", "Closed"};
+    JComboBox filterComboBox = new JComboBox(ProjectState.values());
+    JLabel whatToDo = new JLabel("What you want to do with project?");
+    JRadioButton startJob = new JRadioButton("Start job on the project");
+    JRadioButton endJob = new JRadioButton("End job on the project");
+    JButton executeJob = new JButton("Execute");
+    JLabel projectNameLabel = new JLabel("Project Name:");
+    JTextField projectName = new JTextField();
+    JLabel developersCountLabel = new JLabel("Developers Count:");
 
-    JComboBox filterComboBox = new JComboBox(projectStates);
+    JTextField developersCountField = new JTextField();
 
-    JRadioButton startJob = new JRadioButton("start job on Project");
-    JRadioButton endJob = new JRadioButton("End job on project");
-    JButton executeJob = new JButton("Proceed");
+    JLabel descriptionLabel = new JLabel("Project Description:");
+    JTextField projectDescriptionField = new JTextField();
+    JLabel statusLabel = new JLabel("Status:");
+    JTextField statusField = new JTextField();
 
+    JLabel commentsLabel = new JLabel("Comments:");
+    JTextArea commentListArea = new JTextArea();
 
     public ManagementScreen(Boolean isManager){
 
 
-        getRootPane().setBorder(BorderFactory.createMatteBorder(4,4,4,4, Color.GREEN));
+        getRootPane().setBorder(BorderFactory.createMatteBorder(4,4,4,4, Color.BLACK));
         mainContainer.setLayout(new BorderLayout(8,6));
-        mainContainer.setBackground(Color.GREEN);
+        mainContainer.setBackground(Color.BLACK);
         mainContainer.add(topMenuPanel,BorderLayout.NORTH);
         topMenuPanel.setBackground(Color.YELLOW);
         scrollPaneProjektow.add(listaProjektow);
         mainContainer.add(projectInfoPanel, BorderLayout.CENTER);
-        projectInfoPanel.setBackground(Color.RED);
         mainContainer.add(commentPanel,BorderLayout.SOUTH);
         mainContainer.add(panelListyProjektow, BorderLayout.WEST);
         panelListyProjektow.setLayout(new BorderLayout());
@@ -55,6 +66,24 @@ public class ManagementScreen extends JFrame {
         commentArea.setPreferredSize(new Dimension(800,100));
         commentPanel.add(commentArea);
         commentPanel.add(commentButton);
+        projectInfoPanel.setLayout(new GridLayout(6,2));
+        projectInfoPanel.add(projectNameLabel);
+        projectInfoPanel.add(projectName);
+        projectName.setEditable(false);
+        projectInfoPanel.add(developersCountLabel);
+        projectInfoPanel.add(developersCountField);
+        projectInfoPanel.add(descriptionLabel);
+        projectInfoPanel.add(projectDescriptionField);
+        projectInfoPanel.add(statusLabel);
+        projectInfoPanel.add(statusField);
+        projectInfoPanel.add(commentsLabel);
+        projectInfoPanel.add(commentListArea);
+        decisionPanel.setLayout(new GridLayout(3,0));
+        decisionPanel.add(whatToDo);
+        decisionPanel.add(startJob);
+        decisionPanel.add(endJob);
+        decisionPanel.add(executeJob);
+
 
 
         if(isManager){
@@ -71,7 +100,7 @@ public class ManagementScreen extends JFrame {
 
 
         if(!isManager) {
-
+            projectInfoPanel.add(decisionPanel);
             for (Project p : Project.getProjectList()) {
 
                 model.addElement(p);
---
+++ src/Project/Comment.java
@@ -0,0 +1,21 @@
+package Project;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Comment{
+
+    private String comment;
+    private String author;
+    private String date;
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+    LocalDateTime now = LocalDateTime.now();
+
+    public Comment(String comment, String author) {
+        this.comment = comment;
+        this.author = author;
+        this.date = dtf.format(now);
+
+
+    }
+}
